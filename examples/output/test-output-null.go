93a3ee6df06a36627370f05cdc7a035dee60b9ea
John <johndaligault@gmail.com>
2019-03-21 15:04:17 +0100

    adding usfull examples and image for documentation

--- examples/output/test-output-null.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/DrSmithFr/go-console/pkg/output"
-
-func main() {
-	out := output.NewNullOutput(true, nil)
-	out.Writeln("Ceci est un test")
-	out.Write("Ceci est un test")
-}
