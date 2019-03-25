b8fb70b8a2ccec3fec7cc7ee292d1f7c4eba580e
John <johndaligault@gmail.com>
2019-03-25 17:51:16 +0100

    adding InputDefinition

--- pkg/input/input-definition.go
+++ pkg/input/input-definition.go
@@ -5,22 +5,22 @@ import (
 	"fmt"
 	"github.com/DrSmithFr/go-console/pkg/input/argument"
 	"github.com/DrSmithFr/go-console/pkg/input/option"
+	"strings"
 )
 
-func NewInputDefinition(arguments []argument.InputArgument, options []option.InputOption) *InputDefinition {
+// constructor
+func NewInputDefinition() *InputDefinition {
 	def := &InputDefinition{
 		arguments: map[string]argument.InputArgument{},
 		options:   map[string]option.InputOption{},
 
-		requiredCount: -1,
+		requiredCount: 0,
 
 		hasOptional:        false,
 		hasAnArrayArgument: false,
 
-		shortcuts: []string{},
+		shortcuts: map[string]string{},
 	}
-
-	def.AddArguments(arguments)
 
 	return def
 }
@@ -35,27 +35,30 @@ type InputDefinition struct {
 	hasOptional        bool
 	hasAnArrayArgument bool
 
-	shortcuts []string
+	shortcuts map[string]string
 }
 
 // Sets the InputArgument objects.
-func (i *InputDefinition) SetArguments(arguments []argument.InputArgument) {
+func (i *InputDefinition) SetArguments(arguments []argument.InputArgument) *InputDefinition {
 	i.arguments = map[string]argument.InputArgument{}
 	i.requiredCount = 0
 	i.hasOptional = false
 	i.hasAnArrayArgument = false
 	i.AddArguments(arguments)
+	return i
 }
 
 // Adds an array of InputArgument objects.
-func (i *InputDefinition) AddArguments(arguments []argument.InputArgument) {
+func (i *InputDefinition) AddArguments(arguments []argument.InputArgument) *InputDefinition {
 	for _, arg := range arguments {
 		i.AddArgument(arg)
 	}
+
+	return i
 }
 
 // panic when incorrect argument is given
-func (i *InputDefinition) AddArgument(arg argument.InputArgument) {
+func (i *InputDefinition) AddArgument(arg argument.InputArgument) *InputDefinition {
 	if _, ok := i.arguments[arg.GetName()]; ok {
 		panic(errors.New(fmt.Sprintf("an argument with name '%s' already exists", arg.GetName())))
 	}
@@ -73,12 +76,14 @@ func (i *InputDefinition) AddArgument(arg argument.InputArgument) {
 	}
 
 	if arg.IsRequired() {
-		i.requiredCount += 1
+		i.requiredCount = i.requiredCount + 1
 	} else {
 		i.hasOptional = true
 	}
 
 	i.arguments[arg.GetName()] = arg
+
+	return i
 }
 
 // Returns true if an InputArgument object exists by name or position.
@@ -99,14 +104,8 @@ func (i *InputDefinition) GetArgument(name string) *argument.InputArgument {
 	return nil
 }
 
-func (i *InputDefinition) GetArguments() []argument.InputArgument {
-	var args []argument.InputArgument
-
-	for _, val := range i.arguments {
-		args = append(args, val)
-	}
-
-	return args
+func (i *InputDefinition) GetArguments() map[string]argument.InputArgument {
+	return i.arguments
 }
 
 // Returns the number of InputArguments.
@@ -120,33 +119,200 @@ func (i *InputDefinition) GetArgumentRequiredCount() int {
 }
 
 // Gets the default values.
-func (i *InputDefinition) getArgumentDefaults() map[string]string {
-	values := make(map[string]string)
+func (i *InputDefinition) GetArgumentDefaults() map[string][]string {
+	values := make(map[string][]string)
 
 	for _, arg := range i.arguments {
-		values[arg.GetName()] = arg.GetDefault()
+		if arg.IsArray() {
+			values[arg.GetName()] = arg.GetDefaults()
+		} else {
+			if "" != arg.GetDefault() {
+				values[arg.GetName()] = []string{arg.GetDefault()}
+			} else {
+				values[arg.GetName()] = []string{}
+			}
+		}
+
 	}
 
 	return values
 }
 
 // sets the InputOption objects.
-func (i *InputDefinition) SetOptions(options []option.InputOption) {
+func (i *InputDefinition) SetOptions(options []option.InputOption) *InputDefinition {
 	i.options = make(map[string]option.InputOption)
-	i.shortcuts = []string{}
+	i.shortcuts = make(map[string]string)
 	i.AddOptions(options)
+	return i
 }
 
 // Adds an array of InputOption objects.
-func (i *InputDefinition) AddOptions(options []option.InputOption) {
+func (i *InputDefinition) AddOptions(options []option.InputOption) *InputDefinition {
 	for _, opt := range options {
 		i.AddOption(opt)
 	}
+
+	return i
 }
 
 // panic when option given already exist
-func (i *InputDefinition) AddOption(opt option.InputOption) {
-	if _, ok := i.arguments[opt.GetName()]; ok {
+func (i *InputDefinition) AddOption(opt option.InputOption) *InputDefinition {
+	if _, ok := i.options[opt.GetName()]; ok {
 		panic(errors.New(fmt.Sprintf("an option named '%s' already exists", opt.GetName())))
 	}
+
+	if "" != opt.GetShortcut() {
+		for _, shortcut := range strings.Split(opt.GetShortcut(), "|") {
+			if i.HasShortcut(shortcut) && ! opt.Equals(*i.GetOptionForShortcut(shortcut)) {
+				panic(errors.New(fmt.Sprintf("An option with shortcut '%s' already exists", shortcut)))
+			}
+		}
+	}
+
+	i.options[opt.GetName()] = opt
+
+	if "" != opt.GetShortcut() {
+		for _, shortcut := range strings.Split(opt.GetShortcut(), "|") {
+			i.shortcuts[shortcut] = opt.GetName()
+		}
+	}
+
+	return i
+}
+
+// Returns true if an InputArgument object exists by name or position.
+func (i *InputDefinition) HasOption(name string) bool {
+	_, found := i.options[name]
+	return found
+}
+
+func (i *InputDefinition) GetOption(name string) *option.InputOption {
+	if ! i.HasOption(name) {
+		panic(errors.New(fmt.Sprintf("the '%s' argument does not exist", name)))
+	}
+
+	if arg, ok := i.options[name]; ok {
+		return &arg
+	}
+
+	return nil
+}
+
+func (i *InputDefinition) GetOptions() map[string]option.InputOption {
+	return i.options
+}
+
+// returns true if an InputOption object exists by shortcut.
+func (i *InputDefinition) HasShortcut(s string) bool {
+	_, found := i.shortcuts[s]
+	return found
+}
+
+// Gets an InputOption by shortcut.
+func (i *InputDefinition) GetOptionForShortcut(s string) *option.InputOption {
+	return i.GetOption(i.ShortcutToName(s))
+}
+
+// Gets the default values.
+func (i *InputDefinition) GetOptionDefaults() map[string][]string {
+	values := make(map[string][]string)
+
+	for _, opt := range i.options {
+		if opt.IsArray() {
+			values[opt.GetName()] = opt.GetDefaults()
+		} else {
+			if "" != opt.GetDefault() {
+				values[opt.GetName()] = []string{opt.GetDefault()}
+			} else {
+				values[opt.GetName()] = []string{}
+			}
+		}
+
+	}
+
+	return values
+}
+
+// Returns the InputOption name given a shortcut.
+func (i *InputDefinition) ShortcutToName(s string) string {
+	opt, found := i.shortcuts[s]
+
+	if ! found {
+		panic(errors.New(fmt.Sprintf("the '-%s' option does not exist", s)))
+	}
+
+	return opt
+}
+
+// Returns the InputOption name given a shortcut.
+func (i *InputDefinition) GetSynopsis(short bool) string {
+	var elements []string
+
+	if short && 0 != len(i.GetOptions()) {
+		elements = append(elements, "[options]")
+	} else if !short {
+		for _, opt := range i.options {
+			value := ""
+
+			start := ""
+			end := ""
+
+			if opt.IsValueOptional() {
+				start = "["
+				end = "]"
+			}
+
+			if opt.AcceptValue() {
+				value = fmt.Sprintf(
+					" %s%s%s",
+					start,
+					strings.ToUpper(opt.GetName()),
+					end,
+				)
+			}
+
+			shortcut := ""
+
+			if "" != opt.GetShortcut() {
+				shortcut = fmt.Sprintf("-s|", opt.GetShortcut())
+			}
+
+			elements = append(
+				elements,
+				fmt.Sprintf(
+					"[%s--%s%s]",
+					shortcut,
+					opt.GetName(),
+					value,
+				),
+			)
+		}
+	}
+
+	if 0 != len(elements) && 0 != len(i.GetArguments()) {
+		elements = append(elements, "[--]")
+	}
+
+	tail := ""
+
+	for _, arg := range i.arguments {
+		element := fmt.Sprintf("<%s>", arg.GetName())
+
+		if arg.IsArray() {
+			element = fmt.Sprintf("%s...", element)
+		}
+
+		if !arg.IsRequired() {
+			element = fmt.Sprintf("[%s", element)
+			tail = fmt.Sprintf("%s]", tail)
+		}
+
+		elements = append(elements, element)
+	}
+
+	return fmt.Sprintf(
+		"%s%s",
+		strings.Join(elements, " "),
+		tail,
+	)
 }
---
+++ tests/input/input-definition_test.go
@@ -0,0 +1,456 @@
+package input
+
+import (
+	"github.com/DrSmithFr/go-console/pkg/input"
+	"github.com/DrSmithFr/go-console/pkg/input/argument"
+	"github.com/DrSmithFr/go-console/pkg/input/option"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var arguments = map[string]argument.InputArgument{
+	"foo":  * argument.NewInputArgument("foo", argument.OPTIONAL),
+	"bar":  * argument.NewInputArgument("bar", argument.OPTIONAL),
+	"foo1": * argument.NewInputArgument("foo", argument.OPTIONAL),
+	"foo2": * argument.NewInputArgument("foo2", argument.REQUIRED),
+}
+
+var options = map[string]option.InputOption{
+	"foo": * option.
+		NewInputOption("foo", option.OPTIONAL).
+		SetShortcut("f"),
+	"bar": * option.
+		NewInputOption("bar", option.OPTIONAL).
+		SetShortcut("b"),
+	"foo1": * option.
+		NewInputOption("fooBis", option.OPTIONAL).
+		SetShortcut("f"),
+	"foo2": * option.
+		NewInputOption("foo", option.OPTIONAL).
+		SetShortcut("p"),
+	"multi": * option.
+		NewInputOption("multi", option.OPTIONAL).
+		SetShortcut("m|mm|mmm"),
+}
+
+func TestConstructorArguments(t *testing.T) {
+	def1 := input.NewInputDefinition()
+	assert.Equal(t, map[string]argument.InputArgument{}, def1.GetArguments())
+	assert.Equal(t, map[string]option.InputOption{}, def1.GetOptions())
+}
+
+func TestSetArguments(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		SetArguments([]argument.InputArgument{
+			arguments["foo"],
+		})
+
+	assert.Equal(
+		t,
+		map[string]argument.InputArgument{
+			"foo": arguments["foo"],
+		},
+		def.GetArguments(),
+	)
+
+	def.SetArguments([]argument.InputArgument{
+		arguments["bar"],
+	})
+
+	assert.Equal(
+		t,
+		map[string]argument.InputArgument{
+			"bar": arguments["bar"],
+		},
+		def.GetArguments(),
+	)
+}
+
+func TestAddArguments(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddArguments([]argument.InputArgument{
+			arguments["foo"],
+		})
+
+	assert.Equal(
+		t,
+		map[string]argument.InputArgument{
+			"foo": arguments["foo"],
+		},
+		def.GetArguments(),
+	)
+
+	def.AddArguments([]argument.InputArgument{
+		arguments["bar"],
+	})
+
+	assert.Equal(
+		t,
+		map[string]argument.InputArgument{
+			"foo": arguments["foo"],
+			"bar": arguments["bar"],
+		},
+		def.GetArguments(),
+	)
+}
+
+func TestAddArgument(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddArgument(arguments["foo"])
+
+	assert.Equal(
+		t,
+		map[string]argument.InputArgument{
+			"foo": arguments["foo"],
+		},
+		def.GetArguments(),
+	)
+
+	def.AddArgument(arguments["bar"])
+
+	assert.Equal(
+		t,
+		map[string]argument.InputArgument{
+			"foo": arguments["foo"],
+			"bar": arguments["bar"],
+		},
+		def.GetArguments(),
+	)
+}
+
+func TestArgumentsMustHaveDifferentNames(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddArgument(arguments["foo"]).
+			AddArgument(arguments["foo1"])
+	})
+}
+
+func TestArrayArgumentHasToBeLast(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddArgument(*argument.NewInputArgument("fooarray", argument.IS_ARRAY)).
+			AddArgument(*argument.NewInputArgument("anotherbar", argument.OPTIONAL))
+	})
+}
+
+func TestRequiredArgumentCannotFollowAnOptionalOne(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddArgument(arguments["foo"]).
+			AddArgument(arguments["foo2"])
+	})
+}
+
+func TestGetArgument(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddArgument(arguments["foo"])
+
+	assert.Equal(t, arguments["foo"], *def.GetArgument("foo"))
+}
+
+func TestGetInvalidArgument(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddArgument(arguments["foo"]).
+			GetArgument("bar")
+	})
+}
+
+func TestHasArgument(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddArgument(arguments["foo"])
+
+	assert.True(t, def.HasArgument("foo"))
+	assert.False(t, def.HasArgument("bar"))
+}
+
+func TestGetArgumentRequiredCount(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddArgument(arguments["foo2"])
+
+	assert.Equal(t, 1, def.GetArgumentRequiredCount())
+
+	def.AddArgument(arguments["foo"])
+
+	assert.Equal(t, 1, def.GetArgumentRequiredCount())
+}
+
+func TestGetArgumentCount(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddArgument(arguments["foo2"])
+
+	assert.Equal(t, 1, def.GetArgumentCount())
+
+	def.AddArgument(arguments["foo"])
+
+	assert.Equal(t, 2, def.GetArgumentCount())
+}
+
+func TestGetArgumentDefaults(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		SetArguments([]argument.InputArgument{
+			*argument.
+				NewInputArgument("foo1", argument.OPTIONAL),
+
+			*argument.
+				NewInputArgument("foo2", argument.OPTIONAL).
+				SetDefault("default"),
+
+			*argument.
+				NewInputArgument("foo3", argument.OPTIONAL|argument.IS_ARRAY),
+		})
+
+	validation := map[string][]string{
+		"foo1": {},
+		"foo2": {"default"},
+		"foo3": {},
+		"foo4": {"1", "2"},
+	}
+
+	assert.Equal(t, validation["foo1"], def.GetArgumentDefaults()["foo1"])
+	assert.Equal(t, validation["foo2"], def.GetArgumentDefaults()["foo2"])
+	assert.Equal(t, validation["foo3"], def.GetArgumentDefaults()["foo3"])
+
+	def2 := input.
+		NewInputDefinition().
+		SetArguments([]argument.InputArgument{
+			*argument.
+				NewInputArgument("foo4", argument.OPTIONAL|argument.IS_ARRAY).
+				SetDefaults([]string{"1", "2"}),
+		})
+
+	assert.Equal(t, validation["foo4"], def2.GetArgumentDefaults()["foo4"])
+}
+
+func TestSetOptions(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		SetOptions([]option.InputOption{
+			options["foo"],
+		})
+
+	assert.Equal(
+		t,
+		map[string]option.InputOption{
+			"foo": options["foo"],
+		},
+		def.GetOptions(),
+	)
+
+	def.SetOptions([]option.InputOption{
+		options["bar"],
+	})
+
+	assert.Equal(
+		t,
+		map[string]option.InputOption{
+			"bar": options["bar"],
+		},
+		def.GetOptions(),
+	)
+}
+
+func TestSetOptionsClearsOptions(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			SetOptions([]option.InputOption{
+				options["bar"],
+			}).
+			GetOptionForShortcut("f")
+	})
+}
+
+func TestAddOptions(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOptions([]option.InputOption{
+			options["foo"],
+		})
+
+	assert.Equal(
+		t,
+		map[string]option.InputOption{
+			"foo": options["foo"],
+		},
+		def.GetOptions(),
+	)
+
+	def.AddOptions([]option.InputOption{
+		options["bar"],
+	})
+
+	assert.Equal(
+		t,
+		map[string]option.InputOption{
+			"foo": options["foo"],
+			"bar": options["bar"],
+		},
+		def.GetOptions(),
+	)
+}
+
+func TestAddOption(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOption(options["foo"])
+
+	assert.Equal(
+		t,
+		map[string]option.InputOption{
+			"foo": options["foo"],
+		},
+		def.GetOptions(),
+	)
+
+	def.AddOption(options["bar"])
+
+	assert.Equal(
+		t,
+		map[string]option.InputOption{
+			"foo": options["foo"],
+			"bar": options["bar"],
+		},
+		def.GetOptions(),
+	)
+}
+
+func TestAddDuplicateOption(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddOption(options["foo"]).
+			AddOption(options["foo2"])
+	})
+}
+
+func TestAddDuplicateShortcutOption(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddOption(options["foo"]).
+			AddOption(options["foo1"])
+	})
+}
+
+func TestGetOption(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOption(options["foo"])
+
+	assert.Equal(t, options["foo"], *def.GetOption("foo"))
+}
+
+func TestGetInvalidOption(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			AddOption(options["foo"]).
+			GetOption("bar")
+	})
+}
+
+func TestHasOption(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOption(options["foo"])
+
+	assert.True(t, def.HasOption("foo"))
+	assert.False(t, def.HasOption("BAR"))
+}
+
+func TestHasShortcut(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOption(options["foo"])
+
+	assert.True(t, def.HasShortcut("f"))
+	assert.False(t, def.HasShortcut("b"))
+}
+
+func TestGetOptionForShortcut(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOption(options["foo"])
+
+	assert.Equal(t, options["foo"], *def.GetOptionForShortcut("f"))
+}
+
+func TestGetOptionForMultiShortcut(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		AddOption(options["multi"])
+
+	assert.Equal(t, options["multi"], *def.GetOptionForShortcut("m"))
+	assert.Equal(t, options["multi"], *def.GetOptionForShortcut("mm"))
+	assert.Equal(t, options["multi"], *def.GetOptionForShortcut("mmm"))
+}
+
+func TestGetOptionForInvalidShortcut(t *testing.T) {
+	assert.Panics(t, func() {
+		input.
+			NewInputDefinition().
+			GetOptionForShortcut("l")
+	})
+}
+
+func TestGetOptionDefaults(t *testing.T) {
+	def := input.
+		NewInputDefinition().
+		SetOptions([]option.InputOption{
+			*option.
+				NewInputOption("foo1", option.NONE),
+
+			*option.
+				NewInputOption("foo2", option.REQUIRED),
+
+			*option.
+				NewInputOption("foo3", option.REQUIRED).
+				SetDefault("default"),
+
+			*option.
+				NewInputOption("foo4", option.OPTIONAL),
+
+			*option.
+				NewInputOption("foo5", option.OPTIONAL).
+				SetDefault("default"),
+
+			*option.
+				NewInputOption("foo6", option.OPTIONAL|option.IS_ARRAY),
+
+			*option.
+				NewInputOption("foo7", option.OPTIONAL|option.IS_ARRAY).
+				SetDefaults([]string{"1", "2"}),
+		})
+
+	validation := map[string][]string{
+		"foo1": {},
+		"foo2": {},
+		"foo3": {"default"},
+		"foo4": {},
+		"foo5": {"default"},
+		"foo6": {},
+		"foo7": {"1", "2"},
+	}
+
+	assert.Equal(t, validation["foo1"], def.GetOptionDefaults()["foo1"])
+	assert.Equal(t, validation["foo2"], def.GetOptionDefaults()["foo2"])
+	assert.Equal(t, validation["foo3"], def.GetOptionDefaults()["foo3"])
+	assert.Equal(t, validation["foo4"], def.GetOptionDefaults()["foo4"])
+	assert.Equal(t, validation["foo5"], def.GetOptionDefaults()["foo5"])
+	assert.Equal(t, validation["foo6"], def.GetOptionDefaults()["foo6"])
+	assert.Equal(t, validation["foo7"], def.GetOptionDefaults()["foo7"])
+}
